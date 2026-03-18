d780593caa4f30073dda658366102116000af988
Lim <bigmanmin1@naver.com>
2026-03-18 16:28:14 +0900

    feat: LockOn
    keyinput: Q tap

--- Project_Eden/Public/Characters/GP_PlayerCharacter.h
+++ Project_Eden/Public/Characters/GP_PlayerCharacter.h
@@ -26,6 +26,15 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Camera")
 	TObjectPtr<UCameraComponent> FollowCamera;
-
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Combat|LockOn")
+	bool bIsLockOn = false; // lockOn 주석 한글로 외안되는거야이야
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Combat|LockOn")
+	TObjectPtr<AActor> TargetActor; // lockOn Target
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat|LockOn")
+	float LockOnRotationInterpSpeed = 10.0f;// lockOn 카메라 드래그 구현용
 
 };
